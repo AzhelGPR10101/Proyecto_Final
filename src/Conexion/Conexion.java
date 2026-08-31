2441885c8d4a77f679d5a5602292012b44dac8ea
Gerard Adrian Perez Reyes <gerard.perez.est@tecazuay.edu.ec>
2026-08-31 00:42:01 -0500

    Mover configuracion de base de datos a config.properties (para poder usar Neon en produccion)

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -1,17 +1,25 @@
 package Conexion;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Conexion {
 
-    private static final String HOST = "localhost";
-    private static final String PUERTO = "5432";
-    private static final String BASE_DATOS = "KryptonBase";
-    private static final String URL = "jdbc:postgresql://" + HOST + ":" + PUERTO + "/" + BASE_DATOS;
-    private static final String USUARIO = "postgres";
-    private static final String PASSWORD = "2212";
+    private static Properties cargarConfiguracion() {
+        Properties props = new Properties();
+        try (FileInputStream in = new FileInputStream("config.properties")) {
+            props.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException("No se encontro config.properties en la raiz del proyecto. "
+                    + "Copia config.properties.example, renombralo a config.properties y coloca ahi "
+                    + "los datos de conexion a la base de datos.", e);
+        }
+        return props;
+    }
 
     public static Connection getConnection() throws SQLException {
         try {
@@ -19,6 +27,16 @@ public class Conexion {
         } catch (ClassNotFoundException e) {
             throw new SQLException("No se encontro el driver de PostgreSQL.", e);
         }
-        return DriverManager.getConnection(URL, USUARIO, PASSWORD);
+
+        Properties config = cargarConfiguracion();
+        String host = config.getProperty("db.host", "localhost");
+        String puerto = config.getProperty("db.port", "5432");
+        String baseDatos = config.getProperty("db.name", "KryptonBase");
+        String usuario = config.getProperty("db.user", "postgres");
+        String password = config.getProperty("db.password", "");
+        String sslmode = config.getProperty("db.sslmode", "disable");
+
+        String url = "jdbc:postgresql://" + host + ":" + puerto + "/" + baseDatos + "?sslmode=" + sslmode;
+        return DriverManager.getConnection(url, usuario, password);
     }
 }
